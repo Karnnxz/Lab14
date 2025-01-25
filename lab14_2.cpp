b51f9e24ced6e6bfe2f34775d07f73e5ac50f269
karnnxz <garnt49@gmail.com>
2025-01-25 19:33:33 +0700

    lab14_2.cpp completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -4,9 +4,12 @@ using namespace std;
 const int N = 5;
 
 void inputMatrix(double A[][N]) {
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
+    for (int i = 0; i < N; i++) {
+        cout << "Row " << i + 1 << ": ";
+        for (int j = 0; j < N; j++) {
             cin >> A[i][j];
+        }
+    }
 }
 
 void findLocalMax(const double A[][N], bool B[][N]) {
@@ -16,30 +19,21 @@ void findLocalMax(const double A[][N], bool B[][N]) {
 
     for (int i = 1; i < N-1; i++) {
         for (int j = 1; j < N-1; j++) {
-            if (A[i][j] > A[i-1][j] && A[i][j] > A[i+1][j] && A[i][j] > A[i][j-1] && A[i][j] > A[i][j+1]) {
+            if (A[i][j] >= A[i-1][j] && A[i][j] >= A[i+1][j] && A[i][j] >= A[i][j-1] && A[i][j] >= A[i][j+1]) {
                 B[i][j] = true;
             }
         }
     }
 }
 
+
 void showMatrix(const bool B[][N]) {
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++)
+        for (int j = 0; j < N; j++) {
             cout << B[i][j] << " ";
+        }
         cout << endl;
     }
-}
-
-int main(){
-    double A[N][N]; 
-    bool B[N][N]; 
-    cout << "Input the matrix.....\n";
-    inputMatrix(A);
-    findLocalMax(A, B);
-    cout << "Result is \n";
-    showMatrix(B);
-    return 0;
 }
 
 int main(){
